0cfdc5ec110dc95b112fc6da5307454c1856807a
Monzy <monzy.wandurraga@cun.edu.co>
2025-02-22 14:45:43 -0500

    configuraciones

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ const App = () => {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
-               <Route path="*" element={<Navigate to="/" />} />
+               <Route path="*" element={<Home />} />
             </Routes>
           </div>
         </div>
